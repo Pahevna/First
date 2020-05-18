8380540ef06f80ccbdf50239ffb5b62e2555e0f4
Pahevna <afpavel@inbox.ru>
2020-05-18 20:53:57 +0400

    button and label programmatically

--- Anchors/Anchors/ViewController.swift
+++ Anchors/Anchors/ViewController.swift
@@ -9,12 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    let label: UILabel = {
-        let label = UILabel()
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    }()
+    
     
     let button: UIButton = {
         let button = UIButton(type: .system)
@@ -24,34 +19,41 @@ class ViewController: UIViewController {
         return button
     }()
     
+    let label: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
       
-        view.addSubview(label)
         view.addSubview(button)
+        view.addSubview(label)
         
         setupLabel ()
         setupButton ()
-       
+        
     }
 
-    func setupLabel() {
-        label.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 130).isActive = true
-        label.topAnchor.constraint(equalTo: view.topAnchor, constant: 120).isActive = true
-        label.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 2/3 ).isActive = true
-        label.heightAnchor.constraint(equalToConstant: 80).isActive = true
-    }
-    
     func setupButton() {
         button.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 100).isActive = true
         button.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -20).isActive = true
         button.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 1/2 ).isActive = true
         button.heightAnchor.constraint(equalToConstant: 80).isActive = true
+        button.addTarget(self, action: #selector(pressedButton), for: UIControl.Event.touchUpInside)
         
 }
+   func setupLabel() {
+       label.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 130).isActive = true
+       label.topAnchor.constraint(equalTo: view.topAnchor, constant: 120).isActive = true
+       label.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 2/3 ).isActive = true
+       label.heightAnchor.constraint(equalToConstant: 80).isActive = true
+   }
    
-    func pressedButton(_ sender: UIButton) {
-        label.text = "Hello World"
+    @objc func pressedButton(_ sender: UIButton) {
+         label.text = "Hello World"
+        
     }
     
 }
